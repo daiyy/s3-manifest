9cceb73ab5c1c4194e206bcbee09c117cc6506a5
daiyy <daiyiyang@hotmail.com>
2024-01-07 03:06:06 +0000

    add doc and mark feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
+//! Strongly-typed manifest structs used by Amazon S3 in Rust
+//!
+//! #### Example
+//! ```ignore
+//!     let data = include_bytes!("manifest.json");
+//!
+//!     // slice to struct
+//!     let parsed: InventoryManifest = serde_json::from_slice(data).unwrap();
+//!     // struct to string
+//!     let output: String = serde_json::to_string(&parsed).unwrap();
+//! ```
+
+/// definitions for S3 Inventory
+#[cfg(feature = "inventory")]
 pub mod inventory;
